26760a241c60199d9feb8f61a000a9e059d4569b
Vikas <gaudvikas.1053@gmail.com>
2020-09-26 16:21:28 +0530

    Flutter Address Book

---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Contacts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Contact> contacts = [];
+  List<Contact> contactsFiltered = [];
+  Map<String, Color> contactsColorMap = new Map();
+  TextEditingController searchController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getPermissions();
+  }
+
+  getPermissions() async {
+    if (await Permission.contacts.request().isGranted) {
+      getAllContacts();
+      searchController.addListener(() {
+        filterContacts();
+      });
+    }
+  }
+
+  String flattenPhoneNumber(String phoneStr) {
+    return phoneStr.replaceAllMapped(RegExp(r'^(\+)|\D'), (Match m) {
+      return m[0] == "+" ? "+" : "";
+    });
+  }
+
+  getAllContacts() async {
+    List colors = [Colors.green, Colors.indigo, Colors.yellow, Colors.orange];
+    int colorIndex = 0;
+    List<Contact> data = [];
+    List<Contact> _contacts = (await ContactsService.getContacts(withThumbnails: false)).toList();
+    _contacts.forEach((e) {
+      if (e.displayName.isNotEmpty && e.phones.isNotEmpty) {
+        Color baseColor = colors[colorIndex];
+        contactsColorMap[e.displayName] = baseColor;
+        colorIndex++;
+        if (colorIndex == colors.length) {
+          colorIndex = 0;
+        }
+        data.add(e);
+      }
+    });
+
+    setState(() {
+      contacts = data;
+    });
+  }
+
+  filterContacts() {
+    List<Contact> _contacts = [];
+    _contacts.addAll(contacts);
+    if ( searchController.text!=null && searchController.text.isNotEmpty && searchController.text.length>1) {
+      _contacts.retainWhere((contact) {
+        String searchTerm = searchController.text.toLowerCase();
+        String searchTermFlatten = flattenPhoneNumber(searchTerm);
+        String contactName = contact.displayName.toLowerCase();
+        bool nameMatches = contactName.contains(searchTerm);
+        if (nameMatches == true) {
+          return true;
+        }
+
+        if (searchTermFlatten.isEmpty) {
+          return false;
+        }
+
+        var phone = contact.phones.firstWhere((phn) {
+          String phnFlattened = flattenPhoneNumber(phn.value);
+          return phnFlattened.contains(searchTermFlatten);
+        }, orElse: () => null);
+
+        return phone != null;
+      });
+    }
+    setState(() {
+      contactsFiltered = _contacts;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSearching = searchController.text.isNotEmpty;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                    labelText: 'Search',
+                    border: new OutlineInputBorder(
+                        borderSide: new BorderSide(
+                            color: Theme.of(context).primaryColor)),
+                    prefixIcon: Icon(Icons.search,
+                        color: Theme.of(context).primaryColor)),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: isSearching == true
+                    ? contactsFiltered.length
+                    : contacts.length,
+                itemBuilder: (context, index) {
+                  Contact contact = isSearching == true
+                      ? contactsFiltered[index]
+                      : contacts[index];
+
+                  var baseColor =
+                      contactsColorMap[contact.displayName] as dynamic;
+
+                  Color color1 = baseColor[800];
+                  Color color2 = baseColor[400];
+                  return ListTile(
+                      title: Text(contact.displayName),
+                      subtitle: Text(contact.phones.length > 0
+                          ? contact.phones.elementAt(0).value
+                          : ''),
+                      leading: (contact.avatar != null &&
+                              contact.avatar.length > 0)
+                          ? CircleAvatar(
+                              backgroundImage: MemoryImage(contact.avatar),
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        color1,
+                                        color2,
+                                      ],
+                                      begin: Alignment.bottomLeft,
+                                      end: Alignment.topRight)),
+                              child: CircleAvatar(
+                                  child: Text(contact.initials(),
+                                      style: TextStyle(color: Colors.white)),
+                                  backgroundColor: Colors.transparent)));
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
